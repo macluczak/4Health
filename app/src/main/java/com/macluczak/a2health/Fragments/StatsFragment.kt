45454f652e13ce6add3b661fe4bd58fd38e75629
maciekluczak <maciekluczak2000@gmail.com>
2022-08-21 19:34:54 +0200

    init statistics fragment

---
+++ app/src/main/java/com/macluczak/a2health/Fragments/StatsFragment.kt
@@ -0,0 +1,26 @@
+package com.macluczak.a2health.Fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.macluczak.a2health.R
+import com.macluczak.a2health.databinding.FragmentStatsBinding
+
+
+class StatsFragment : Fragment(R.layout.fragment_stats) {
+    lateinit var binding: FragmentStatsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentStatsBinding.bind(view)
+    }
+
+
+}
